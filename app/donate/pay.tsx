98317bd5dc53d9b06299dd6d039efeccd277bedc
arjundubeyg <reboostify@gmail.com>
2025-06-23 17:32:42 +0530

    d

--- app/donate/pay.tsx
+++ app/donate/pay.tsx
@@ -109,6 +109,7 @@ export default function RazorpayPayment({
       <Script 
         src="https://checkout.razorpay.com/v1/checkout.js" 
         strategy="lazyOnload"
+        
       />
 
       <button 
