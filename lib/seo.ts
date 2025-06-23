b2984af425ed617e620f433804177b10ab7c3d25
arjundubeyg <reboostify@gmail.com>
2025-06-23 13:31:47 +0530

    d

--- lib/seo.ts
+++ lib/seo.ts
@@ -1,7 +1,7 @@
 // lib/seo.ts
 export const siteConfig = {
   name: "Arjun Dubey",
-  description: "Arjun Dubey",
+  description: "Arjun Dubey , arjundubey , arjundubey , Arjun Kumar Dubey , ArjunKumarDubey , arjun",
   url: "https://www.arjundubey.com/",
   ogImage: "https://www.arjundubey.com/images/arjun.png",
   creator: "@yourusername",
