2f62393b5ebd635f73921eb51c01986109e100b8
Arjun Kumar Dubey <arjunkumardubey@MacBook-Air.local>
2025-10-24 02:00:53 +0530

    d

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -5,7 +5,7 @@ export default function DownloadPage() {
     <div className="min-h-screen flex flex-col items-center px-4" style={{ paddingTop: '30vh' }}>
       <h1 className="text-3xl md:text-2xl font-semibold mb-12 md:mb-8 text-center">deArKs Version 9</h1>
       <a
-        href="https://mkw3xpovahzkrpq2.public.blob.vercel-storage.com/product/app-debug.apk"
+        href="https://mkw3xpovahzkrpq2.public.blob.vercel-storage.com/product/deArKs.apk"
         download
         className="inline-flex items-center gap-3 px-8 py-4 md:px-6 md:py-3 border border-gray-300 rounded-lg hover:border-gray-400 transition-colors text-lg md:text-base"
       >
